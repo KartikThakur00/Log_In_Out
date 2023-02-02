5958234d6a85fec3d928fa607c5ded5c40917b3b
Kartik Thakur <76154014+KartikThakur00@users.noreply.github.com>
2023-02-03 02:45:10 +0530

    commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const MongoDBStore= require("connect-mongo")(session);
 
 
 
-const dbUrl = "mongodb+srv://admin:grjtcDUO9NbS1uDq@cluster0.88q8xcj.mongodb.net/?retryWrites=true&w=majority"
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/app' ;
 mongoose.connect(dbUrl,{
      useNewUrlParser:true,
      //useCreateIndex:true,
